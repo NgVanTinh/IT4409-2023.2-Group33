0edd185128d7b0d45f8102917d37a51a3e7298e4
MBP_THUAN <pdoanthuannb96@gmail.com>
2024-04-08 21:29:33 +0700

    Init SpringBoot
    Add base application

---
+++ back_end/group33/src/main/java/it4409/group33/Group33Application.java
@@ -0,0 +1,13 @@
+package it4409.group33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group33Application.class, args);
+	}
+
+}
---
+++ back_end/group33/src/test/java/it4409/group33/Group33ApplicationTests.java
@@ -0,0 +1,13 @@
+package it4409.group33;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group33ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
