e5a89df2f6f2df8d35a7e270a623d881ed2e0806
Nguyen Quang Thuan <thuan13112003@gmail.com>
2024-04-29 01:45:52 +0700

    Navbar, cart, show Sidebar, search item, logo branch

--- front_end/client/src/components/Navbar/Navbar.js
+++ front_end/client/src/components/Navbar/Navbar.js
@@ -1,5 +1,71 @@
 import React from "react";
+import "./Navbar.scss";
+import { Link } from "react-router-dom";
+import {
+  FaBars,
+  FaBagShopping,
+  FaMagnifyingGlass,
+  FaCartShopping,
+} from "react-icons/fa6";
+// solve sidebar
+import { setSidebarOn } from "../../store/sidebarSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  const dispatch = useDispatch();
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-cnt flex align-center">
+        <div className="brand-and-toggler flex align-center">
+          <button
+            onClick={() => dispatch(setSidebarOn())}
+            className="sidebar-show-btn text-white"
+          >
+            <FaBars />
+          </button>
+
+          <Link to={"/"} className="navbar-brand flex align-center">
+            <span className="navbar-brand-ico">
+              <FaBagShopping />
+            </span>
+            <span className="navbar-brand-txt mx-2">Shopping</span>
+          </Link>
+        </div>
+
+        <div className="navbar-collapse w-100">
+          <div className="navbar-search bg-white">
+            <div className="flex align-center">
+              <input
+                type="text"
+                className="form-control fs-14"
+                placeholder="Tìm kiếm sản phẩm"
+              />
+              <Link
+                to={"/"}
+                className="text-white search-btn flex align-center justify-center"
+              >
+                <FaMagnifyingGlass />
+              </Link>
+            </div>
+          </div>
+
+          <ul className="navbar-nav flex align-center fs-12 fw-4 font-manrope">
+            <li className="nav-item no-wrap">
+              <Link to={"/"} className="nav-link text-capitalize">
+                Category here
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="navbar-cart flex align-center">
+          <Link to="/cart" className="cart-btn">
+            <FaCartShopping />
+            <div className="cart-items-value">0</div>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 }
