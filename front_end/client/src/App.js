7199f4d9d48e9d93884c7231ea8cf2b5e398d700
Nguyen Quang Thuan <thuan13112003@gmail.com>
2024-05-02 22:14:57 +0700

    slick home page

--- front_end/client/src/App.js
+++ front_end/client/src/App.js
@@ -25,6 +25,9 @@ function App() {
           <BrowserRouter>
             <Header />
             <Sidebar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
           </BrowserRouter>
         </Provider>
       </div>
