98cb67b60477b5265851c7447efc6153cf72eec7
Nguyen Quang Thuan <thuan13112003@gmail.com>
2024-05-14 22:24:30 +0700

    Viết các sự kiện xử lý cho Cart và in số lượng sản phẩm trong giỏ hàng ra giao diện

--- front_end/client/src/components/Navbar/Navbar.js
+++ front_end/client/src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Navbar.scss";
 import { Link } from "react-router-dom";
 import {
@@ -12,10 +12,22 @@ import { setSidebarOn } from "../../store/sidebarSlice";
 import { useSelector, useDispatch } from "react-redux";
 // get all categories
 import { getAllCategories } from "../../store/categorySlice";
+// carts
+import {
+  getAllCarts,
+  getCartItemsCount,
+  getCartTotal,
+} from "../../store/cartSlice";
 
 export default function Navbar() {
   const dispatch = useDispatch();
   const categories = useSelector(getAllCategories);
+  const carts = useSelector(getAllCarts);
+  const itemsCount = useSelector(getCartItemsCount);
+
+  useEffect(() => {
+    dispatch(getCartTotal());
+  }, [carts]);
 
   return (
     <nav className="navbar">
@@ -23,55 +35,55 @@ export default function Navbar() {
         <div className="brand-and-toggler flex align-center">
           <button
             onClick={() => dispatch(setSidebarOn())}
+            type="button"
             className="sidebar-show-btn text-white"
           >
             <FaBars />
           </button>
-
-          <Link to={"/"} className="navbar-brand flex align-center">
+          <Link to="/" className="navbar-brand flex align-center">
             <span className="navbar-brand-ico">
               <FaBagShopping />
             </span>
-            <span className="navbar-brand-txt mx-2">Shopping</span>
+            <span className="navbar-brand-txt mx-2">
+              <span className="fw-7">Shopping</span>
+            </span>
           </Link>
         </div>
-
         <div className="navbar-collapse w-100">
           <div className="navbar-search bg-white">
             <div className="flex align-center">
               <input
                 type="text"
                 className="form-control fs-14"
-                placeholder="Tìm kiếm sản phẩm"
+                placeholder="Search your preferred items here"
               />
               <Link
-                to={"/"}
+                to=""
                 className="text-white search-btn flex align-center justify-center"
               >
                 <FaMagnifyingGlass />
               </Link>
             </div>
           </div>
-
           <ul className="navbar-nav flex align-center fs-12 fw-4 font-manrope">
-            {categories.length > 0 &&
-              categories.slice(0, 8).map((category, idx) => (
-                <li className="nav-item no-wrap">
-                  <Link
-                    to={`category/${category}`}
-                    className="nav-link text-capitalize"
-                  >
-                    {category.replace("-", " ")}
-                  </Link>
-                </li>
-              ))}
+            {/* taking only first 8 categories */}
+            {categories.slice(0, 8).map((category, idx) => (
+              <li className="nav-item no-wrap" key={idx}>
+                <Link
+                  to={`category/${category}`}
+                  className="nav-link text-capitalize"
+                >
+                  {category.replace("-", " ")}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
 
         <div className="navbar-cart flex align-center">
           <Link to="/cart" className="cart-btn">
             <FaCartShopping />
-            <div className="cart-items-value">0</div>
+            <div className="cart-items-value">{itemsCount}</div>
           </Link>
         </div>
       </div>
