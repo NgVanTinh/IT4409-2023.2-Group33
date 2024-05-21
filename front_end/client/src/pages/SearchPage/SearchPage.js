3652d7160f110125496fc06e5d8ce88d7fd49fe6
Nguyen Quang Thuan <thuan13112003@gmail.com>
2024-05-21 18:25:40 +0700

    Xây dựng giao diện trang SearchPage

--- front_end/client/src/pages/SearchPage/SearchPage.js
+++ front_end/client/src/pages/SearchPage/SearchPage.js
@@ -1,14 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./SearchPage.scss";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { STATUS } from "../../utils/status";
 import Loader from "../../components/Loader/Loader";
 import ProductList from "../../components/ProductList/ProductList";
+import {
+  getAllSearchProducts,
+  getAllSearchProductsStatus,
+  fetchAsyncSearchProducts,
+  setSearchKeyword,
+} from "../../store/searchSlice";
 
 export default function SearchPage() {
   const dispatch = useDispatch();
   const { searchKeyword } = useParams();
-  console.log(searchKeyword);
-  return <div></div>;
+  const searchProducts = useSelector(getAllSearchProducts);
+  const searchProductsStatus = useSelector(getAllSearchProductsStatus);
+
+  useEffect(() => {
+    dispatch(fetchAsyncSearchProducts(searchKeyword));
+  }, [searchKeyword]);
+
+  if (searchProducts.length === 0) {
+    return (
+      <div className="container">
+        <h3>Không tìm thấy sản phẩm</h3>
+      </div>
+    );
+  }
+
+  return (
+    <main>
+      <div className="search-content bg-whitesmoke">
+        <div className="container">
+          <div className="py-5">
+            <div className="title-md">
+              <h3>Kết quả tìm kiếm cho "{searchKeyword}"</h3>
+            </div>
+            <br />
+            {searchProductsStatus === STATUS.LOADING ? (
+              <Loader />
+            ) : (
+              <ProductList products={searchProducts} />
+            )}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
--- front_end/client/src/store/store.js
+++ front_end/client/src/store/store.js
@@ -2,14 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import sidebarReducer from "./sidebarSlice";
 import categoryReducer from "./categorySlice";
 import productReducer from "./productSlice";
-import cartSlice from "./cartSlice";
+import cartReducer from "./cartSlice";
+import searchReducer from "./searchSlice";
 
 const store = configureStore({
   reducer: {
     sidebar: sidebarReducer,
     category: categoryReducer,
     product: productReducer,
-    cart: cartSlice,
+    cart: cartReducer,
+    search: searchReducer,
   },
 });
 
