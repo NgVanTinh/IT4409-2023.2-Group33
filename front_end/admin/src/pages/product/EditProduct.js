2cd37c5ac7111a860ba856e2d1ae7b6fb7556b78
Zinne0811 <nguyen.tinh.8113@gmail.com>
2024-05-25 04:01:55 +0700

    done edit product

--- front_end/admin/src/pages/product/EditProduct.js
+++ front_end/admin/src/pages/product/EditProduct.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 import IconButton from '@mui/material/IconButton';
 import { Button } from "@mui/material";
 import TextField from '@mui/material/TextField';
@@ -10,6 +13,9 @@ import TopHeader from "../../components/TopHeader";
 import { styled } from '@mui/material/styles';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import CancelSharpIcon from '@mui/icons-material/CancelSharp';
+import CancelPresentationOutlinedIcon from '@mui/icons-material/CancelPresentationOutlined';
+import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+import InputAdornment from '@mui/material/InputAdornment';
 
 const VisuallyHiddenInput = styled('input')({
   clip: 'rect(0 0 0 0)',
@@ -52,10 +58,23 @@ const EditProduct = () => {
 
   const onImageChange = (e) => {
     const files = Array.from(e.target.files);
+    if (product.images.length < 5) {
     setProduct(prevProduct => ({
       ...prevProduct,
       images: [...prevProduct.images, ...files]
     }));
+    } else {
+      toast.warn('Can not upload more than 5 images!', {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        });
+    }
   };
 
    const handleDeleteImage = (index) => {
@@ -174,179 +193,177 @@ const EditProduct = () => {
           />
         </Grid>
         <Grid item sm={8}>
-          <p 
-            style={{
-              color: 'blue',
-              width: '100px',
-              marginLeft: '10px'
-            }}
-          >Thumbnail</p>
-          <div>
+           <TextField
+              disabled
+              fullWidth
+              label="Thumbnail"
+              multiline
+              rows={5}
+              InputLabelProps={{ style: { color: 'blue' } }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    {isString(product.thumbnail) ? (
+                      <img
+                        src={product.thumbnail}
+                        alt="Product Thumbnail"
+                        style={{
+                          width: '100px',
+                          height: '100px',
+                          borderRadius: '10px',
+                          border: '1px solid blue',
+                          marginRight: '10px',
+                          marginBottom: '10px',
+                        }}
+                      />
+                    ) : (
+                      <img
+                        src={URL.createObjectURL(product.thumbnail)}
+                        alt="Product Thumbnail"
+                        style={{
+                          width: '100px',
+                          height: '100px',
+                          borderRadius: '10px',
+                          border: '1px solid blue',
+                          marginRight: '10px',
+                          marginBottom: '10px',
+                        }}
+                      />
+                    )}
 
-            {isString(product.thumbnail) 
-            ? (  
-              <div
-                style={{position: 'relative', display: 'inline-block'}}
-              >
-
-                <img
-                src={product.thumbnail}
-                alt="Product Thumbnail"
-                style={{
-                    width: '100px',
-                    height: '100px',
-                    borderRadius: '10px',
-                    border: '1px solid blue',
-                    marginRight: '10px',
-                    marginBottom: '10px',
-                  }}
-                />
-
-              </div>
-              
-            ) 
-            : (  
-              <div
-                style={{position: 'relative', display: 'inline-block'}}
-              >
-                <img
-                  src={URL.createObjectURL(product.thumbnail)}
-                  alt="Product Thumbnail"
-                   style={{
-                    width: '100px',
-                    height: '100px',
-                    borderRadius: '10px',
-                    border: '1px solid blue',
-                    marginRight: '10px',
-                    marginBottom: '10px',
-                  }}
-                /> 
-
-              </div>
-              )}
-               
-            <Button
-              component="label"
-              role={undefined}
-              variant="contained"
-              tabIndex={-1}
-              startIcon={<CloudUploadIcon />}
-              
-            >
-              Upload file
-              <VisuallyHiddenInput type="file" accept="image/*"  onChange={onThumbnailChange} />
-            </Button>
-          </div>
-          
+                     <Button
+                      component="label"
+                      variant="contained"
+                      startIcon={<CloudUploadIcon />}
+                    >
+                      Upload file
+                      <input type="file" accept="image/*" onChange={onThumbnailChange} style={{ display: 'none' }} />
+                    </Button>
+                  </InputAdornment>
+                ),
+              }}
+            />
         </Grid>
 
         <Grid item sm={8}>
-          <p 
-            style={{
-              color: 'blue',
-              width: '100px',
-              marginLeft: '10px'
-            }}
-          >Images</p>
-
-          <div>
-           {product.images && product.images.map((image, index) => (
-            isString(image) ? (
-              <div
-                  style={{position: 'relative', display: 'inline-block'}}
-               >
-                <IconButton
-                  size="small"
-                  onClick={() => handleDeleteImage(index)}
-                  style={{
-                    position: 'absolute',
-                    bottom: '80%',
-                    right: '0',
-                    color: 'grey',
-                  }}
-                >
-                  <CancelSharpIcon />
-                </IconButton>
-                <img
-                  src={image}
-                  alt="Product Thumbnail"
-                  style={{
-                      width: '100px',
-                      height: '100px',
-                      borderRadius: '20px',
-                      border: '1px solid blue',
-                      marginRight: '10px',
-                    }}
-                  />
-                
-              </div>
-              ) 
-            : (  
-              <div
-                style={{position: 'relative', display: 'inline-block'}}
-              >
-                <IconButton
-                  size="small"
-                  onClick={() => handleDeleteImage(index)}
-                  style={{
-                    position: 'absolute',
-                    bottom: '80%',
-                    right: '0',
-                    color: 'grey',
-                  }}
-                >
-                  <CancelSharpIcon />
-                </IconButton>
-                <img
-                  src={URL.createObjectURL(image)}
-                  alt="Product Thumbnail"
-                   style={{
-                    width: '100px',
-                    height: '100px',
-                    borderRadius: '20px',
-                    border: '1px solid blue',
-                    marginRight: '10px',
-                  }}
-                />
-              </div>
-            )
-            ))}
-            <br/>
-            <Button
-              component="label"
-              role={undefined}
-              variant="contained"
-              tabIndex={-1}
-              startIcon={<CloudUploadIcon />}
-              sx={{mt:2}}
-              
-            >
-              Upload file
-              <VisuallyHiddenInput type="file" accept="image/*" onChange={onImageChange} />
-            </Button>
-          </div>
+           <TextField
+              disabled
+              fullWidth
+              label="Images"
+              multiline
+              rows={5}
+              InputLabelProps={{ style: { color: 'blue' } }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    {product.images && product.images.map((image, index) => (
+                      isString(image) ? (
+                        <div
+                            style={{position: 'relative', display: 'inline-block'}}
+                        >
+                          <IconButton
+                            size="small"
+                            onClick={() => handleDeleteImage(index)}
+                            style={{
+                              position: 'absolute',
+                              bottom: '80%',
+                              right: '0',
+                              color: 'grey',
+                            }}
+                          >
+                            <CancelSharpIcon />
+                          </IconButton>
+                          <img
+                            src={image}
+                            alt="Product Thumbnail"
+                            style={{
+                                width: '100px',
+                                height: '100px',
+                                borderRadius: '20px',
+                                border: '1px solid blue',
+                                marginRight: '10px',
+                              }}
+                            />
+                          
+                        </div>
+                        ) 
+                      : (  
+                        <div
+                          style={{position: 'relative', display: 'inline-block'}}
+                        >
+                          <IconButton
+                            size="small"
+                            onClick={() => handleDeleteImage(index)}
+                            style={{
+                              position: 'absolute',
+                              bottom: '80%',
+                              right: '0',
+                              color: 'grey',
+                            }}
+                          >
+                            <CancelSharpIcon />
+                          </IconButton>
+                          <img
+                            src={URL.createObjectURL(image)}
+                            alt="Product Thumbnail"
+                            style={{
+                              width: '100px',
+                              height: '100px',
+                              borderRadius: '20px',
+                              border: '1px solid blue',
+                              marginRight: '10px',
+                            }}
+                          />
+                        </div>
+                      )
+                      ))}
+                      <Button
+                        component="label"
+                        role={undefined}
+                        variant="contained"
+                        tabIndex={-1}
+                        startIcon={<CloudUploadIcon />}
+                        sx={{mt:2}}
+                        
+                      >
+                        Upload file
+                        <VisuallyHiddenInput type="file" accept="image/*" onChange={onImageChange} />
+                      </Button>
+                  </InputAdornment>
+                ),
+              }}
+            />
         </Grid>
+        <ToastContainer/>
         
         
-      </Grid>
+    </Grid>
         
-      <div
-        style={{
-            display: 'flex',
-            justifyContent: 'flex-end',  
-            marginTop: '20px',
-            marginBottom: '20px',
-            marginRight: '20px',
-            marginLeft: '20px',
-            width: '100px',        
-          }}
+       <div
+      style={{
+          display: 'flex',
+          justifyContent: 'flex-end',  
+          marginTop: '20px',
+          marginBottom: '20px',
+          marginRight: '20px',
+          marginLeft: '600px',
+        }}
+    >
+      <Button
+        sx={{backgroundColor: 'red', color: 'white', borderRadius: '10px', mr:1}}
+        onClick={() => {navigate('/admin/products')}}
       >
-        <Button 
+        <CancelPresentationOutlinedIcon sx={{mr:1}}/>
+        Cancel
+      </Button>
+      <Button 
         type="submit"
-        >
-          Submit
-        </Button>
-      </div>
+        sx={{backgroundColor: '#2686CB', color: 'white', borderRadius: '10px', marginLeft: 'auto'}}
+      >
+        <SendOutlinedIcon sx={{mr:1}} />
+        Submit
+      </Button>
+    </div>
        
     </Box>
     </>
