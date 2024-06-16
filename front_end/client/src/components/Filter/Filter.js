aa59db39cf137133747c63e1c9dcf49b925775f4
Nguyen Quang Thuan <thuan13112003@gmail.com>
2024-06-16 22:55:42 +0700

    Bộ lọc theo category

--- front_end/client/src/components/Filter/Filter.js
+++ front_end/client/src/components/Filter/Filter.js
@@ -2,6 +2,7 @@ import React from "react";
 import FilterSort from "./FilterSort";
 import FilterRange from "./FilterRange";
 import FilterRating from "./FilterRating";
+import FilterCategory from "./FilterCategory";
 
 export default function Filter() {
   return (
@@ -9,6 +10,7 @@ export default function Filter() {
       <FilterSort />
       <FilterRange />
       <FilterRating />
+      <FilterCategory />
     </>
   );
 }
---
+++ front_end/client/src/components/Filter/FilterCategory.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { resetFilter, setFilter } from "../../store/productSlice";
+import {
+  fetchAsyncCategories,
+  getAllCategories,
+} from "../../store/categorySlice";
+import { Select } from "antd";
+
+// Destructuring để lấy component Option từ Select
+const { Option } = Select;
+
+const FilterCategory = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAsyncCategories());
+  }, [dispatch]);
+
+  const categories = useSelector(getAllCategories);
+
+  const handleCategoryChange = (value) => {
+    if (value) dispatch(setFilter({ filterType: "category", value }));
+    else dispatch(resetFilter({ filterType: "category" }));
+  };
+
+  return (
+    <Select
+      style={{ width: 200 }}
+      placeholder="Lựa chọn danh mục"
+      onChange={handleCategoryChange}
+      allowClear
+    >
+      <Option value="">Tất cả danh mục</Option>
+      {categories.map((category, index) => (
+        <Option key={index} value={category.name}>
+          {category.name}
+        </Option>
+      ))}
+    </Select>
+  );
+};
+
+export default FilterCategory;
